0c91e743cfe1d4ee44140b1579852840ad27f52c
nmrtkhs <nmrtkhs@gmail.com>
2018-10-09 21:32:51 +0900

    Fix touch event

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -12,8 +12,17 @@ export default class Helloworld extends cc.Component {
     @property
     text: string = 'hello';
 
+    @property(cc.Node)
+    canvas: cc.Node = null;
+
+    onLoad() {
+        this.canvas.on(cc.Node.EventType.TOUCH_START, function (event) {
+            var touches = event.getTouches();
+            this.titleImage.enabled = false;
+            this.label.enabled = false;
+        }, this);
+    }
+
     start () {
-        // init logic
-        this.label.string = this.text;
     }
 }
