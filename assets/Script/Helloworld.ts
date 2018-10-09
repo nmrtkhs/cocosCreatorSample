61b6e90753aff96fcd412970fe1b521d16e516c3
nmrtkhs <nmrtkhs@gmail.com>
2018-10-09 10:03:17 +0900

    First commit

---
+++ assets/Script/Helloworld.ts
@@ -0,0 +1,19 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Helloworld extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property(cc.Sprite)
+    titleImage: cc.Sprite = null;
+
+    @property
+    text: string = 'hello';
+
+    start () {
+        // init logic
+        this.label.string = this.text;
+    }
+}
